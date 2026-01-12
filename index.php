0a63beebeabe57526584accd0ee1561b2ffaad92
Cintia <cintiakauana186@gmail.com>
2026-01-12 08:41:30 -0300

    Primeiro comando

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário notas</title>
+</head>
+<body>
+    <!-- FORM -->
+    <form action="resultado.php" method="post" enctype = "multipart/form-data">
+        <!-- NOME -->
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" placeholder="Digite seu nome">                                                                                                       <br><br>
+
+        <!-- IDADE -->
+        <label for="idade">Idade:</label>
+        <input type="number" name="idade" placeholder="Digite sua idade">                                                                                                   <br><br>
+        
+        <!-- NOTA -->
+        <label for="nota">Nota:</label>
+        <input type="number" name="nota" placeholder="Digite sua nota">                                                                                                     <br><br>
+        
+        
+        <!--BOTÃO-->
+          
+    <a href="<?php include_once("resultado.php");?>"><input type="submit" name = "enviar"> </a>
+
+    </form>
+</body>
+</html>
